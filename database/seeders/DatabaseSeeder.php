676a53e1fcba7439d5d5254aa261448458f0900e
smazziotta <mazziottastefano@gmail.com>
2024-01-26 00:44:12 -0300

    [dev] Create "Country" entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Country;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Country::factory(10)->create();
     }
 }
