460959f3db5e12de67376b4275638462e21fcf02
stefano-mazziotta <mazziottastefano@gmail.com>
2024-01-31 22:31:21 -0300

    [Location] create factory logic

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Location;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,14 +12,23 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class LocationFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Location::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->city,
+            'coordinates' => $this->faker->latitude . ',' . $this->faker->longitude,
+            'country_id' => Country::all()->random()->id,
         ];
     }
 }
