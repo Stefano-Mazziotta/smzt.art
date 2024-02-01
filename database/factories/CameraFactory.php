e1860326eac432c8f4af51de366150ae5ef3a1cd
stefano-mazziotta <mazziottastefano@gmail.com>
2024-01-31 22:30:53 -0300

    [Camera] create factory logic

--- database/factories/CameraFactory.php
+++ database/factories/CameraFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Camera;
+use App\Models\Country;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -10,14 +12,24 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CameraFactory extends Factory
 {
     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Camera::class;
+
+    /**
      * Define the model's default state.
      *
-     * @return array<string, mixed>
+     * @return array
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'production_year' => $this->faker->year(),
+            'country_id' => Country::all()->random()->id,
         ];
     }
+
 }
