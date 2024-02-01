04750a673248b20106f52bc4760e244fd642edb7
stefano-mazziotta <mazziottastefano@gmail.com>
2024-01-31 22:31:11 -0300

    [Label] create factory logic

--- database/factories/LabelFactory.php
+++ database/factories/LabelFactory.php
@@ -3,12 +3,15 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Label;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Label>
  */
 class LabelFactory extends Factory
 {
+    
+     protected $model = Label::class;
     /**
      * Define the model's default state.
      *
@@ -16,8 +19,9 @@ class LabelFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
-            //
+            'name' => $this->faker->word(),
         ];
     }
 }
