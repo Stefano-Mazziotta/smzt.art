1c10e6f675d0e0ea09ffe8b29bba9b1f6af185b3
stefano-mazziotta <mazziottastefano@gmail.com>
2024-02-09 13:56:27 -0300

    Handle exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +28,30 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+    
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+
+            $errors = $exception->validator->errors();
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Ops! Some errors occurred',
+                'errors' => $errors
+            ], 422);
+        }
+
+        return parent::render($request, $exception);
     }
 }
